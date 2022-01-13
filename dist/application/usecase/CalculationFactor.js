003995773a276b6fe5dcd52858b46ce4a748a252
jeanriffel <jeanriffel1@gmail.com>
2022-01-12 22:30:36 -0300

    new files

---
+++ dist/application/usecase/CalculationFactor.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CalculationFactor = void 0;
+class CalculationFactor {
+    getFactorFreight() {
+        return 300;
+    }
+}
+exports.CalculationFactor = CalculationFactor;
