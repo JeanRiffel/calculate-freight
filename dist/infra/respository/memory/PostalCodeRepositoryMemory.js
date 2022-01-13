a6d011816776ed7b25750be8fa9318710c5b26fa
jeanriffel <jeanriffel1@gmail.com>
2022-01-12 22:30:36 -0300

    feat(adding) adding new features

---
+++ dist/infra/respository/memory/PostalCodeRepositoryMemory.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PostalCodeRepositoryMemory = void 0;
+const PostalCodeOutPut_1 = require("../../../application/dto/PostalCodeOutPut");
+class PostalCodeRepositoryMemory {
+    constructor() {
+        this.listPostalCode = [{ "id": 1, "code": 89897856, "city": "Sao Paulo", "value": 15.58 },
+            { "id": 2, "code": 85497856, "city": "Rio de Janeiro", "value": 10.66 },
+            { "id": 3, "code": 89878856, "city": "Curtiba", "value": 9.25 },
+            { "id": 4, "code": 98952856, "city": "Belo Horizonte", "value": 5.58 }
+        ];
+    }
+    convertResult(postalCodeResult) {
+        const postalCode = new PostalCodeOutPut_1.PostalCodeOutPut;
+        postalCode.id = postalCodeResult.id;
+        postalCode.code = postalCodeResult.code;
+        postalCode.city = postalCodeResult.city;
+        postalCode.value = postalCodeResult.value;
+        return postalCode;
+    }
+    findByPostalCode(postalCode) {
+        let postalCodeResult = this.listPostalCode.find(item => item.code === postalCode);
+        if (!postalCodeResult)
+            throw new Error('Postal code not found');
+        return this.convertResult(postalCodeResult);
+    }
+}
+exports.PostalCodeRepositoryMemory = PostalCodeRepositoryMemory;
---
+++ dist/src/infra/web/server.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const index_route_1 = __importDefault(require("./routes/index-route"));
+const server = (0, express_1.default)();
+server.use(index_route_1.default);
+exports.default = server;
