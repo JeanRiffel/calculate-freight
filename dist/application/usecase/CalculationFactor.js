618388b0163588ac06c983a13202d60a9125a7a3
jeanriffel <jeanriffel1@gmail.com>
2022-02-12 19:22:03 -0300

    chore(dist) removig dist folder

--- dist/application/usecase/CalculationFactor.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CalculationFactor = void 0;
-class CalculationFactor {
-    getFactorFreight() {
-        return 300;
-    }
-}
-exports.CalculationFactor = CalculationFactor;
