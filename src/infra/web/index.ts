b7cc54143aeb530af8bfb20eae359ac38d4eab47
jeanriffel <jeanriffel1@gmail.com>
2022-02-12 19:35:56 -0300

    feat(framework) implementing framework for api

--- src/infra/web/index.ts
+++ src/infra/web/index.ts
@@ -1,4 +1,4 @@
 import server from './server';
+const port = 3000;
 
-
-server.listen(3000, ()=>{ console.log('Running') } );
+server.listen(port, ()=>{ console.log( { serverStatus: 'is running', port:port } ) } );
